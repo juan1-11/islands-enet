7e5f6181f2377159e4d3eb0ae1144e4db64c1cfa
juan1-11 <juancruzquiroga2912@gmail.com>
2026-07-14 22:25:45 -0300

    Version 1.0.3

--- src/main/java/com/juan/islands/IslandsEnet.java
+++ src/main/java/com/juan/islands/IslandsEnet.java
@@ -1,5 +1,8 @@
 package com.juan.islands;
 
+import com.juan.islands.block.ModBlocks;
+import com.juan.islands.item.ModItems;
+
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.util.Identifier;
@@ -8,7 +11,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class IslandsEnet implements ModInitializer {
-	public static final String MOD_ID = "islands-enet";
+
+	public static final String MOD_ID = "islands_enet";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -22,6 +26,9 @@ public class IslandsEnet implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		ModBlocks.registerModBlocks();
+        ModItems.registerModItems();
+    
 	}
 
 	public static Identifier id(String path) {
---
+++ src/main/java/com/juan/islands/item/ModItems.java
@@ -0,0 +1,20 @@
+package com.juan.islands.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item RAW_PYRITE = registerItem("raw_pyrite",
+        new Item(new Item.Settings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier("islands_enet", name), item);
+    }
+
+    public static void registerModItems() {
+        // Fuerza la carga de la clase
+    }
+}
